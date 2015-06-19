a25332847ac3f241d155c0cacf18930bd9169322
Daniele Polencic <daniele.polencic@gmail.com>
2015-06-19 13:33:17 +0100

    small text change

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/port', function (req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node.js app is running on port', app.get('port'));
 });
